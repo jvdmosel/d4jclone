618bb7a64332c39a2430e4d9277863a90c8fecf8
jvdmosel <nehit@protonmail.com>
2021-06-27 23:47:44 +0200

    Add unit test compile

--- d4jclone/core/compile.py
+++ d4jclone/core/compile.py
@@ -3,19 +3,10 @@ from pathlib import Path
 from shutil import copyfile, copytree
 from subprocess import PIPE, run
 
-from d4jclone.parser.projectParser import parseProject
-from d4jclone.parser.bugParser import parseBug
-from d4jclone.config import PROJECTDIR
+from d4jclone.parser.checkoutParser import parseCheckout
 from d4jclone.util.formatting import fill
 
-def compile():
-    basedir = Path.cwd()
-    parts = basedir.name.split('_')
-    # TODO: check if working dir is a possible dir
-    project = parseProject(parts[0][:1].upper() + parts[0][1:])
-    project_dir = Path(PROJECTDIR) / project.id
-    bug = parseBug(project.id, parts[1])
-    rev = bug.rev_buggy if parts[2] == 'buggy' else bug.rev_fixed
+def fixBuild(project_dir, basedir, bug, rev):
     if (project_dir / 'project_root.json').is_file():
         with open(project_dir / 'project_root.json') as json_file:
             project_root = json.load(json_file)
@@ -29,12 +20,16 @@ def compile():
         for _file in (project_dir / 'build_files' / rev).glob('*.*'):
             copyfile(_file, basedir / _file.name)
         copytree(project_dir / 'build_files' / rev, basedir, dirs_exist_ok=True)
-    print(fill('Running ant (compile)'), end ='')
-    args = ['ant', '-Dbasedir=' + str(basedir), '-Dprojectdir=' + str(project_dir), '-buildfile', project.build_file, 'compile']
+
+def compile(workdir = None, task = 'compile', arg = 'compile'):
+    workdir = Path(workdir) if workdir != None else Path.cwd
+    checkout = parseCheckout(workdir)
+    fixBuild(checkout.project_dir, workdir, checkout.bug, checkout.rev)
+    print(fill('Running ant (' + task + ')'), end ='')
+    args = ['ant', '-Dbasedir=' + str(workdir), '-Dprojectdir=' + str(checkout.project_dir), '-buildfile', checkout.project.build_file, arg]
     result = run(args, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     if 'BUILD SUCCESSFUL' in result.stdout:
         print('OK')
     else:
-        print(rev)
+        print(checkout.rev)
         print(result.stdout, result.stderr)
-    
