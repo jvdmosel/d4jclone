0ed758ee31b95adcfffd05b2a3066d3f40877d0c
d4jclone <nehit@protonmail.com>
2021-08-06 13:27:42 +0200

    Add triggering tests to info

--- d4jclone/core/info.py
+++ d4jclone/core/info.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from d4jclone.config import ENV
 from d4jclone.parser.bugParser import getModifiedSources, parseBug
 from d4jclone.parser.projectParser import parseProject
+from d4jclone.parser.triggerTestParser import parseTriggerTests
 from d4jclone.util.projects import projects
 
 sep = '\n'+ '-' * 80
@@ -19,16 +20,23 @@ def project_info(project_id):
     print('           Vcs: ' + 'Vcs::' + project.vcs)
     print('    Repository: ' + str(Path(ENV['REPODIR']) / (projects.get(project.id) + '.git')))
     print('     Commit db: ' + str(Path(ENV['SCRIPTDIR']) / 'projects' / project.id / 'bugs.csv'))
-    print('Number of bugs: ' + str(project.number_of_bugs))
+    print('Number of bugs: ' + str(project.number_of_bugs) + sep)
 
 def bug_info(project_id, bug_id):
     bug = parseBug(project_id, bug_id)
-    print('Summary for Bug: ' + project_id + '-' + bug.id + sep)
+    print('\nSummary for Bug: ' + project_id + '-' + str(bug.id) + sep)
     print('Revision ID (fixed version):\n' + bug.rev_fixed + sep)
     print('Revision date (fixed version):\n' + bug.rev_date_fixed + sep)
     print('Bug report id:\n' + bug.external_id + sep)
     print('Bug report url:\n' + bug.url + sep)
-    print('Root cause in triggering tests:\n' + 'UNKNOWN' + sep)
+    trigger_tests = parseTriggerTests(project_id, bug_id)
+    print('Root cause in triggering tests:')
+    if trigger_tests != None:
+        for test in trigger_tests.keys():
+            print(' - ' + test + '\n   --> ' + ' '.join(trigger_tests[test]))
+        print('-' * 80)
+    else:
+        print('UNKNOWN')
     srcs = getModifiedSources(bug)
     if len(srcs) > 0:
         srcs = '\n - '.join(srcs)
@@ -39,11 +47,11 @@ def bug_info(project_id, bug_id):
 def info(project_id, bug_id = None):
     if project_id in projects.keys():
         project = parseProject(project_id)
-        if bug_id == None:
-            project_info(project_id)
-        elif 0 < int(bug_id) <= project.number_of_bugs+1:
-            bug_info(project_id, bug_id)
-        else:
-            raise Exception('Error: ' + project_id + '-' + bug_id  + ' is a non-existent bug')
+        project_info(project_id)
+        if bug_id != None:
+            if 0 < int(bug_id) <= project.number_of_bugs+1:
+                bug_info(project_id, bug_id)
+            else:
+                raise Exception('Error: ' + project_id + '-' + bug_id  + ' is a non-existent bug')
     else:
         raise Exception('Invalid project_id:' + project_id)
