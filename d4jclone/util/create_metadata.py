8a677a43d390d5678eeaf8b8165b4faf705412df
d4jclone <nehit@protonmail.com>
2021-08-09 22:49:10 +0200

    Add repository and bugs.csv to create_metadata

--- d4jclone/util/create_metadata.py
+++ d4jclone/util/create_metadata.py
@@ -2,17 +2,19 @@ import json
 from pathlib import Path
 
 from d4jclone.util.projects import projects
-from d4jclone.config import PROJECTDIR
+from d4jclone.config import ENV
 
 
 def createMetadata():
     for project in projects.keys():
-        path = Path(PROJECTDIR) / project
+        path = Path(ENV['PROJECTDIR']) / project
         metadata = {}
         metadata['id'] = project
         metadata['program'] = projects.get(project)
-        metadata['build_file'] = str(path / (project + '.build.xml'))
-        metadata['vcs'] = 'Git'
+        metadata['build_file'] = project + '.build.xml'
+        metadata['vcs'] = 'Vcs::Git'
+        metadata['repository'] = projects.get(project) + '.git'
+        metadata['bugs.csv'] = project + '/bugs.csv'
         with open(path / 'bugs.csv') as f:
             metadata['number_of_bugs'] = str(sum(1 for line in f) - 1)
         with open(path /  'metadata.json', 'w') as json_file:
