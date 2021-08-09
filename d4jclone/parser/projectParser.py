9e3606185bb2f845fc7a54a1277063a27f566744
d4jclone <nehit@protonmail.com>
2021-08-10 01:10:31 +0200

    Update projectParser to reflect metadata changes

--- d4jclone/parser/projectParser.py
+++ d4jclone/parser/projectParser.py
@@ -3,16 +3,20 @@ from pathlib import Path
 from d4jclone.config import ENV
 
 class Project():
-    def __init__(self, _id, program, vcs, build_file, number_of_bugs):
+    def __init__(self, _id, program, vcs, build_file, number_of_bugs,
+                 repository, bug_db):
         self.id = _id
         self.program = program
         self.vcs = vcs
         self.build_file = build_file
         self.number_of_bugs = number_of_bugs
+        self.repository = repository
+        self.bug_db = bug_db
 
 def parseProject(project):
     metadata_file = Path(ENV['PROJECTDIR']) / project / 'metadata.json'
     with open(metadata_file) as json_file:
         metadata = json.load(json_file)
         return Project(metadata['id'], metadata['program'], metadata['vcs'], 
-                       ENV['PROJECTDIR'] + '/' + metadata['build_file'], int(metadata['number_of_bugs']))
+                       ENV['PROJECTDIR'] + '/' + metadata['build_file'], int(metadata['number_of_bugs']),
+                       ENV['REPODIR'] + '/' + metadata['repository'], ENV['PROJECTDIR'] + '/' + metadata['bugs.csv'])
