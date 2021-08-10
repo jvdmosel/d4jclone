35146231402330aaeb8180d88c348c0c6363700a
d4jclone <nehit@protonmail.com>
2021-08-10 14:50:22 +0200

    FIx bug. Compile now always cleans target before compilation

--- d4jclone/core/compile.py
+++ d4jclone/core/compile.py
@@ -8,6 +8,8 @@ def compile(workdir = None, task = 'compile', arg = 'compile'):
     workdir = Path(workdir) if workdir != None else Path.cwd()
     checkout = parseCheckout(workdir)
     print(fill('Running ant (' + task + ')', '.', 75), end ='')
+    args = ['ant', '-Dbasedir=' + str(workdir), '-Dprojectdir=' + str(checkout.project_dir), '-buildfile', checkout.project.build_file, 'clean']
+    result = run(args, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     args = ['ant', '-Dbasedir=' + str(workdir), '-Dprojectdir=' + str(checkout.project_dir), '-buildfile', checkout.project.build_file, arg]
     result = run(args, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     if 'BUILD SUCCESSFUL' in result.stdout:
