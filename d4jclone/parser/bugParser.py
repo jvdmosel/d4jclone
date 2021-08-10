86e8886c9cf7dd8f10892b9b0f0dde1417e9b81b
d4jclone <nehit@protonmail.com>
2021-08-10 13:51:49 +0200

    Add buggy revision date to bugParser

--- d4jclone/parser/bugParser.py
+++ d4jclone/parser/bugParser.py
@@ -5,11 +5,13 @@ from d4jclone.config import ENV
 
 
 class Bug():
-    def __init__(self, project, _id, rev_buggy, rev_fixed, rev_date_fixed, external_id, url):
+    def __init__(self, project, _id, rev_buggy, rev_fixed, 
+                 rev_date_buggy, rev_date_fixed, external_id, url):
         self.project = project
         self.id = _id
         self.rev_buggy = rev_buggy
         self.rev_fixed = rev_fixed
+        self.rev_date_buggy = rev_date_buggy
         self.rev_date_fixed = rev_date_fixed
         self.external_id = external_id
         self.url = url
@@ -17,7 +19,7 @@ class Bug():
 def parseBug(project, bug_id):
     path = Path(ENV['PROJECTDIR']) / project / 'bugs.csv'
     bug = linecache.getline(str(path), int(bug_id)+1).split(',')
-    return Bug(project, bug_id, bug[1], bug[2], bug[3], bug[4], bug[5][:-1])
+    return Bug(project, bug_id, bug[1], bug[2], bug[3], bug[4], bug[5], bug[6][:-1])
 
 def getModifiedSources(bug):
     srcs = parseLines(bug, 'modified_classes', '.src')
