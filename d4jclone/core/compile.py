0cf71962bea6df2c9630b61a5f44c5395c7f20c9
d4jclone <nehit@protonmail.com>
2021-08-16 18:23:24 +0200

    Change output if compile command fails

--- d4jclone/core/compile.py
+++ d4jclone/core/compile.py
@@ -7,7 +7,7 @@ from d4jclone.util.formatting import fill
 def compile(workdir = None, task = 'compile', arg = 'compile'):
     workdir = Path(workdir) if workdir != None else Path.cwd()
     checkout = parseCheckout(workdir)
-    print(fill('Running ant (' + task + ')', '.', 75), end ='')
+    print(fill('Running ant (' + task + ')', '.', 75), end ='', flush=True)
     args = ['ant', '-Dbasedir=' + str(workdir), '-Dprojectdir=' + str(checkout.project_dir), '-buildfile', checkout.project.build_file, 'clean']
     result = run(args, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     args = ['ant', '-Dbasedir=' + str(workdir), '-Dprojectdir=' + str(checkout.project_dir), '-buildfile', checkout.project.build_file, arg]
@@ -15,5 +15,5 @@ def compile(workdir = None, task = 'compile', arg = 'compile'):
     if 'BUILD SUCCESSFUL' in result.stdout:
         print('OK')
     else:
-        print('FAIL')
+        print('FAILED')
         print(result.stdout, result.stderr)
