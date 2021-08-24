11f50d11bceacfd64d956b9e7efde6fd4c8da7bb
d4jclone <nehit@protonmail.com>
2021-08-24 16:01:55 +0200

    Add custom generated testsuite to test command

--- d4jclone/core/test.py
+++ d4jclone/core/test.py
@@ -1,7 +1,9 @@
 import re
+import subprocess
 from pathlib import Path
 from subprocess import PIPE, run
 
+from d4jclone.config import ENV
 from d4jclone.core.checkout import checkoutVersion
 from d4jclone.core.compile import compile
 from d4jclone.parser.checkoutParser import parseCheckout
@@ -9,13 +11,19 @@ from d4jclone.util.formatting import fill
 
 class_method = re.compile('([^:]+::[^:]+)')
 
-def test(workdir = None, testcase = None, relevant = False, suite = None, monitor = False): 
+def test(workdir = None, testcase = None, relevant = False, testsuite = None, monitor = False): 
     workdir = Path(workdir) if workdir != None else Path.cwd()
-    compile(workdir, 'compile.tests', 'compile-tests')
+    testdir = workdir / '.test_suite'
+    if testsuite:
+        extractTestsuite(testsuite, testdir)
+        compile(workdir, 'compile.gen.tests', 'compile-gen-tests', testdir)
+    else:
+        compile(workdir, 'compile.tests', 'compile-tests')
     checkout = parseCheckout(workdir)
     args = ['ant', 
             '-Dbasedir=' + str(workdir), 
-            '-Dprojectdir=' + str(checkout.project_dir)]
+            '-Dprojectdir=' + str(checkout.project_dir),
+            '-Dlibdir=' + ENV['LIBDIR']]
     if testcase != None and bool(class_method.match(testcase)):
         args.extend(['-Dtest.entry.class=' + testcase.split('::')[0], 
                      '-Dtest.entry.method=' + testcase.split('::')[1]])
@@ -24,7 +32,10 @@ def test(workdir = None, testcase = None, relevant = False, suite = None, monito
     if monitor:
         args.extend(['-Dtest.monitor=True'])
     print(fill('Running ant (test)', '.', 75), end ='')
-    args.extend(['-buildfile', checkout.project.build_file, 'test'])
+    if testsuite:
+        args.extend(['-Dtest.dir=' + str(testdir), '-buildfile', checkout.project.build_file, 'run-gen-tests'])
+    else:
+        args.extend(['-buildfile', checkout.project.build_file, 'test'])
     result = run(args, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     print('OK')
     return result
@@ -38,3 +49,14 @@ def testFails(workdir, testcase, version):
         return True if len(result.stderr) > 0 else False
     else:
         raise Exception('Couldn\'t find directory!')
+    
+def extractTestsuite(testsuite, testdir):
+    if not Path(testsuite).is_file():
+        print('Test suite archive not found: ' + testsuite)
+        return
+    path = Path(testdir)
+    if not path.is_dir():
+        path.mkdir()
+    else:
+        subprocess.call(['rm', '-f', '-r', str(path) + '/*'])
+    subprocess.call(['tar', '-xjf', testsuite, '-C', testdir])
